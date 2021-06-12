feca71a6c1675cc288c041ba7be6f3de05f62e27
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-06-12 11:52:09 -0400

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Alphonso Sensley II on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
